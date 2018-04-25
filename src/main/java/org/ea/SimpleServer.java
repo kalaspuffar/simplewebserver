1aa0997a2f1083a61094c117eec7cb782097a94a
Daniel Persson <mailto.woden@gmail.com>
2018-04-25 08:03:10 +0200

    Updated SimpleServer

--- src/main/java/org/ea/SimpleServer.java
+++ src/main/java/org/ea/SimpleServer.java
@@ -5,12 +5,15 @@ import com.sun.net.httpserver.HttpServer;
 
 
 public class SimpleServer {
+    private static final int SERVER_PORT = 8080;
+
     public static void main(String[] args) {
         try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+            HttpServer server = HttpServer.create(new InetSocketAddress(SERVER_PORT), 0);
             server.createContext("/", new RequestHandler());
             server.setExecutor(null); // creates a default executor
             server.start();
+            System.out.println("Server started at " + SERVER_PORT);
         } catch (Exception e) {
             e.printStackTrace();
         }
